ac8d552cfe9113d5b4dfc91ecd9df00518edf4e0
Evgenia Sivtsova <105357551+EvSivtsova@users.noreply.github.com>
2022-10-29 19:53:19 +0100

    Refactor #apply_bandpass_filter

--- review-2/lib/apply_bandpass_filter.rb
+++ review-2/lib/apply_bandpass_filter.rb
@@ -2,8 +2,17 @@ def apply_bandpass_filter(music_track, lower_limit, upper_limit)
   music_track.map do |frequency|
     if frequency > upper_limit
       upper_limit
+    elsif frequency < lower_limit
+      lower_limit
     else
-      frequency > lower_limit ? frequency : lower_limit
+      frequency
     end
+
+    # return upper_limit if frequency > upper_limit
+    # return lower_limit if frequency < lower_limit
+    # frequency
+    # # return upper_limit if frequency > upper_limit
+    # return lower_limit frequency > lower_limit
+    # frequency
   end
 end
