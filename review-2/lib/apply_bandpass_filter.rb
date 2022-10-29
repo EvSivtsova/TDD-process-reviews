4c90a5f095f35fd363b2c3596ccf6334f3347be4
Evgenia Sivtsova <105357551+EvSivtsova@users.noreply.github.com>
2022-10-29 19:13:26 +0100

    Filter one frequency below lower limit - hardcoded

--- review-2/lib/apply_bandpass_filter.rb
+++ review-2/lib/apply_bandpass_filter.rb
@@ -1,3 +1,9 @@
 def apply_bandpass_filter(music_track, lower_limit, upper_limit)
-  music_track
+  music_track.map do |frequency| 
+    if frequency > lower_limit
+      frequency
+    else
+      20
+    end 
+  end
 end
--- review-2/spec/apply_bandpass_filter_spec.rb
+++ review-2/spec/apply_bandpass_filter_spec.rb
@@ -18,12 +18,22 @@ describe '#bandpass filter' do
       expect(result).to eq [45, 45]
     end
 
-    it 'returns two frequency in an array' do
+    it 'returns multiple frequency in an array' do
       music_track = [45, 45, 45]
       lower_limit = 20
       upper_limit = 50
       result = apply_bandpass_filter(music_track, lower_limit, upper_limit)
       expect(result).to eq [45, 45, 45]
+    end
+  end
+
+  context 'the frequencies are below range' do
+    it 'returns the lower limit as a single frequency in an array' do
+      music_track = [10]
+      lower_limit = 20
+      upper_limit = 50
+      result = apply_bandpass_filter(music_track, lower_limit, upper_limit)
+      expect(result).to eq [20]
     end
   end
 end
