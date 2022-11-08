85b1eb62127599a29c68e30b07c67ea20eab615c
Evgenia Sivtsova <105357551+EvSivtsova@users.noreply.github.com>
2022-11-08 21:02:57 +0000

    Refactor - return resulting_value array

--- review-4/lib/calculate_sum.rb
+++ review-4/lib/calculate_sum.rb
@@ -2,19 +2,15 @@ def calculate_sum(sum_string)
   input_array = sum_string.split(" ")
   resulting_value = [sum_string]
   if sum_string.length == 1
-    resulting_value.push(input_array[0].to_i)
+    result = input_array[0].to_i
   else 
     if input_array[1] == "+"
       result = input_array[0].to_i + input_array[2].to_i
-      resulting_value.push(result)
     elsif input_array[1] == "-"
       result = input_array[0].to_i - input_array[2].to_i
-      resulting_value.push(result)
-    else
-      if input_array[1] == "/"
-        result = input_array[0].to_i / input_array[2].to_i
-        resulting_value.push(result)
-      end
+    elsif input_array[1] == "/"
+      result = input_array[0].to_i / input_array[2].to_i
     end
   end
+  resulting_value.push(result)
 end
