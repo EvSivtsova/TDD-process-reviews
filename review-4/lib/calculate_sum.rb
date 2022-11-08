d3565f003c74cd65671c4252dfea937232fc45e6
Evgenia Sivtsova <105357551+EvSivtsova@users.noreply.github.com>
2022-11-08 23:29:00 +0000

    Refactor - extract evaluation of longers than 1 No strings

--- review-4/lib/calculate_sum.rb
+++ review-4/lib/calculate_sum.rb
@@ -3,20 +3,8 @@ def calculate_sum(sum_string)
   resulting_value = [sum_string]
   if input_array.length == 1
     result = input_array[0].to_f
-  elsif input_array.length == 3
-    result = sum_two_number(input_array)
-  elsif input_array.length == 5
-    array_two_numbers = input_array.slice!(0, 3)
-    intermediary_result = sum_two_number(array_two_numbers)
-    array_two_numbers = [intermediary_result.to_s, input_array].flatten
-    result = sum_two_number(array_two_numbers)
   else
-    array_two_numbers1 = input_array.slice!(0, 3)
-    intermediary_result1 = sum_two_number(array_two_numbers1)
-    array_two_numbers2 = [intermediary_result1.to_s, input_array.slice!(0, 2)].flatten
-    intermediary_result2 = sum_two_number(array_two_numbers2)
-    array_two_numbers3 = [intermediary_result2.to_s, input_array].flatten
-    result = sum_two_number(array_two_numbers3)
+    result = evaluate_array_of_more_than_one_number(input_array)
   end
   result = result.to_i if result.to_s.match?(/\.?0+$/)
   resulting_value.push(result)
@@ -24,7 +12,18 @@ end
 
 private
 
-def sum_two_number(array_two_numbers)
+def evaluate_array_of_more_than_one_number(input_array)
+  array_two_numbers = input_array.slice!(0, 3)
+  result = sum_two_numbers(array_two_numbers)
+  while input_array.length.positive?
+    array_two_numbers = [result.to_s, input_array.slice!(0, 2)].flatten
+    result = sum_two_numbers(array_two_numbers)
+    break if input_array.length.zero?
+  end
+  result
+end
+
+def sum_two_numbers(array_two_numbers)
   number1 = array_two_numbers[0].to_f
   number2 = array_two_numbers[2].to_f
   operator = array_two_numbers[1]
