528157ed132ecab04a081c2a26a4fe98ab6803d1
Evgenia Sivtsova <105357551+EvSivtsova@users.noreply.github.com>
2022-11-08 20:51:28 +0000

    Refactor addition and subtraction of 1or 2 numbers

--- review-4/lib/calculate_sum.rb
+++ review-4/lib/calculate_sum.rb
@@ -1,14 +1,15 @@
 def calculate_sum(sum_string)
+  input_array = sum_string.split(" ")
+  resulting_value = [sum_string]
   if sum_string.length == 1
-    resulting_value = [sum_string, sum_string.to_i]
+    resulting_value.push(input_array[0].to_i)
   else 
-    input_array = sum_string.split(" ")
     if input_array[1] == "+"
       result = input_array[0].to_i + input_array[2].to_i
-      [sum_string, result]
+      resulting_value.push(result)
     elsif input_array[1] == "-"
       result = input_array[0].to_i - input_array[2].to_i
-      [sum_string, result]
+      resulting_value.push(result)
     else 
       ["2 / 1", 2]
     end
