399ff72c7a17effd6b52f49417fdf469017bc22e
Evgenia Sivtsova <105357551+EvSivtsova@users.noreply.github.com>
2022-11-08 21:34:05 +0000

    Refactor - extract sum of two numbers, private

--- review-4/lib/calculate_sum.rb
+++ review-4/lib/calculate_sum.rb
@@ -1,18 +1,24 @@
 def calculate_sum(sum_string)
-  input_array = sum_string.split(" ")
+  input_array = sum_string.split
   resulting_value = [sum_string]
   if sum_string.length == 1
     result = input_array[0].to_i
-  else 
-    if input_array[1] == "+"
-      result = input_array[0].to_i + input_array[2].to_i
-    elsif input_array[1] == "-"
-      result = input_array[0].to_i - input_array[2].to_i
-    elsif input_array[1] == "/"
-      result = input_array[0].to_i / input_array[2].to_i
-    else
-      result = input_array[0].to_i * input_array[2].to_i
-    end
+  else
+    result = sum_two_number(input_array)
   end
   resulting_value.push(result)
+end
+
+private
+
+def sum_two_number(array_two_numbers)
+  if array_two_numbers[1] == '+'
+    array_two_numbers[0].to_i + array_two_numbers[2].to_i
+  elsif array_two_numbers[1] == '-'
+    array_two_numbers[0].to_i - array_two_numbers[2].to_i
+  elsif array_two_numbers[1] == '/'
+    array_two_numbers[0].to_i / array_two_numbers[2].to_i
+  else
+    array_two_numbers[0].to_i * array_two_numbers[2].to_i
+  end
 end
--- review-4/spec/calculate_sum_spec.rb
+++ review-4/spec/calculate_sum_spec.rb
@@ -1,85 +1,85 @@
 require 'calculate_sum'
 
-describe "calculate_sum" do
-  context "if the input is a string with one number" do
-    it "returns 1 if the input is '1'" do
-      input = "1"
+describe 'calculate_sum' do
+  context 'if the input is a string with one number' do
+    it 'returns 1 if the input is "1"' do
+      input = '1'
       result = calculate_sum(input)
-      expect(result).to eq ["1", 1]
+      expect(result).to eq ['1', 1]
     end
 
-    it "returns 2 if the input is '2'" do
-      input = "2"
+    it 'returns 2 if the input is "2"' do
+      input = '2'
       result = calculate_sum(input)
-      expect(result).to eq ["2", 2]
+      expect(result).to eq ['2', 2]
     end
 
-    it "returns 3 if the input is '3'" do
-      input = "3"
+    it 'returns 3 if the input is "3"' do
+      input = '3'
       result = calculate_sum(input)
-      expect(result).to eq ["3", 3]
+      expect(result).to eq ['3', 3]
     end
   end
 
-  context "if the input is a string of sum of two numbers" do
-    it "returns the sum and result if the input is '1 + 1'" do
-      input = "1 + 1"
+  context 'if the input is a string of sum of two numbers' do
+    it 'returns the sum and result if the input is "1 + 1"' do
+      input = '1 + 1'
       result = calculate_sum(input)
-      expect(result).to eq ["1 + 1", 2]
+      expect(result).to eq ['1 + 1', 2]
     end
 
-    it "returns the sum and result if the input is '1 - 1'" do
-      input = "1 - 1"
+    it 'returns the sum and result if the input is "1 - 1"' do
+      input = '1 - 1'
       result = calculate_sum(input)
-      expect(result).to eq ["1 - 1", 0]
+      expect(result).to eq ['1 - 1', 0]
     end
 
-    it "returns the sum and result if the input is '2 - 1'" do
-      input = "2 / 1"
+    it 'returns the sum and result if the input is "2 - 1"' do
+      input = '2 / 1'
       result = calculate_sum(input)
-      expect(result).to eq ["2 / 1", 2]
+      expect(result).to eq ['2 / 1', 2]
     end
 
-    it "returns the sum and result if the input is '2 + 1'" do
-      input = "2 + 1"
+    it 'returns the sum and result if the input is "2 + 1"' do
+      input = '2 + 1'
       result = calculate_sum(input)
-      expect(result).to eq ["2 + 1", 3]
+      expect(result).to eq ['2 + 1', 3]
     end
 
-    it "returns the sum and result if the input is '2 - 1'" do
-      input = "2 - 1"
+    it 'returns the sum and result if the input is "2 - 1"' do
+      input = '2 - 1'
       result = calculate_sum(input)
-      expect(result).to eq ["2 - 1", 1]
+      expect(result).to eq ['2 - 1', 1]
     end
 
-    it "returns the sum and result if the input is '4 / 2'" do
-      input = "4 / 2"
+    it 'returns the sum and result if the input is "4 / 2"' do
+      input = '4 / 2'
       result = calculate_sum(input)
-      expect(result).to eq ["4 / 2", 2]
+      expect(result).to eq ['4 / 2', 2]
     end
 
-    it "returns the sum and result if the input is '6 / 2'" do
-      input = "6 / 2"
+    it 'returns the sum and result if the input is "6 / 2"' do
+      input = '6 / 2'
       result = calculate_sum(input)
-      expect(result).to eq ["6 / 2", 3]
+      expect(result).to eq ['6 / 2', 3]
     end
     
-    it "returns the sum and result if the input is '2 * 2'" do
-      input = "2 * 2"
+    it 'returns the sum and result if the input is "2 * 2"' do
+      input = '2 * 2'
       result = calculate_sum(input)
-      expect(result).to eq ["2 * 2", 4]
+      expect(result).to eq ['2 * 2', 4]
     end
     
-    it "returns the sum and result if the input is '3 * 2'" do
-      input = "3 * 2"
+    it 'returns the sum and result if the input is "3 * 2"' do
+      input = '3 * 2'
       result = calculate_sum(input)
-      expect(result).to eq ["3 * 2", 6]
+      expect(result).to eq ['3 * 2', 6]
     end
 
-    it "returns the sum and result if the input is '3 * 3'" do
-      input = "3 * 3"
+    it 'returns the sum and result if the input is "3 * 3"' do
+      input = '3 * 3'
       result = calculate_sum(input)
-      expect(result).to eq ["3 * 3", 9]
+      expect(result).to eq ['3 * 3', 9]
     end
   end 
 end
